4c2370cc0c478a1dba9df55717ff5f8ecc8a3fb1
amgeraldo14 <amgeraldo14@gmail.com>
2021-08-19 16:46:38 +0700

    styles added

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import Posts from './components/Posts/Posts.js'
+import Form from './components/Form/Form.js'
+import memories from './images/memories.png'
+import useStyles from './styles'
+const App = () => {
+  const classes = useStyles()
+  return (
+    <Container maxwidth='lg'>
+      <AppBar className={ classes.appBar } position="static" color="inherit">
+        <Typography className={ classes.heading } variant="h2" align="center">Memories</Typography>
+        <img src={memories} alt="memories" height="60" className={ classes.image }></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+            <Grid items xs={12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid items xs={12} sm={4}>
+              <Form/>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
+}
+
+export default App
